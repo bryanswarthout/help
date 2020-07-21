fc712049bf86c31f034f414cc52dcd69b6850ce4
bryanswarthout <bryanswarthout@github.com>
2020-07-21 15:59:05 -0700

    Updates

---
+++ precache-manifest.b9d8c60c95fc4f1a30ce3b8ce162ff79.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9a30f0e5a178e9ca60a64729c55ee80f",
+    "url": "/React-Portfoliov2/index.html"
+  },
+  {
+    "revision": "059e55f27d4590d873b7",
+    "url": "/React-Portfoliov2/static/css/main.a6fd0737.chunk.css"
+  },
+  {
+    "revision": "c1e23bfc2ce36a57b0f8",
+    "url": "/React-Portfoliov2/static/js/2.46c2815b.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/React-Portfoliov2/static/js/2.46c2815b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "059e55f27d4590d873b7",
+    "url": "/React-Portfoliov2/static/js/main.ea3bad3f.chunk.js"
+  },
+  {
+    "revision": "2c158a4ef6770a169bfb",
+    "url": "/React-Portfoliov2/static/js/runtime-main.36287ab9.js"
+  },
+  {
+    "revision": "8c68d0778b734fca6f6b710b8a914c62",
+    "url": "/React-Portfoliov2/static/media/Burgerlogger.8c68d077.png"
+  },
+  {
+    "revision": "51279603c7672228d0f216dadcf54010",
+    "url": "/React-Portfoliov2/static/media/S7LT1i.51279603.jpg"
+  },
+  {
+    "revision": "139dd497d53f9849fd63f9d5fccf9cbd",
+    "url": "/React-Portfoliov2/static/media/YouMDb.139dd497.png"
+  },
+  {
+    "revision": "aafc75fb820701e76b0394d195d31560",
+    "url": "/React-Portfoliov2/static/media/aboutme.aafc75fb.jpg"
+  },
+  {
+    "revision": "10807f6757ba754346310c6e85d5c2fd",
+    "url": "/React-Portfoliov2/static/media/outerspacehero.10807f67.jpeg"
+  },
+  {
+    "revision": "46a849f52f4a15a3b7f2884e36f5b752",
+    "url": "/React-Portfoliov2/static/media/spacehubthumbnail.46a849f5.png"
+  },
+  {
+    "revision": "51a5a4f0e29b910e8ed7e2ebb746f93c",
+    "url": "/React-Portfoliov2/static/media/thievesmonopoly.51a5a4f0.png"
+  },
+  {
+    "revision": "9a9955e0f524137b27523cce3cf43756",
+    "url": "/React-Portfoliov2/static/media/weather.9a9955e0.png"
+  },
+  {
+    "revision": "c6e1137707a83cabddc5f4e126c70d81",
+    "url": "/React-Portfoliov2/static/media/workday.c6e11377.png"
+  }
+]);
