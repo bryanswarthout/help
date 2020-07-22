0fd019e5fd222570e3bba842fd67fc18cf9c6337
bryanswarthout <bryanswarthout@github.com>
2020-07-21 18:43:56 -0700

    fix deploy issue again

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './App.css';
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Portfolio from "./pages/Portfolio";
 import About from "./pages/About";
 import ContactMe from "./pages/ContactMe";
@@ -9,16 +9,16 @@ import LandingPage from "./components/LandingPage/LandingPage";
 function App() {
   return (
     <Router>
-      
     <div className="App">
+      <Switch>
             <Route exact path="/" component={LandingPage} />
             <Route exact path="/about" component={About} />
             <Route exact path="/contact" component={ContactMe} />
             <Route exact path="/portfolio" component={Portfolio} />
+      </Switch>
     </div>
     </Router>
   );
 }
 
 export default App;
-;
